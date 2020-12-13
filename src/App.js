8b325e0ab2dda8745c984d34239c668f9a53e095
EC2 Default User <ec2-user@ip-172-31-40-75.ap-south-1.compute.internal>
2020-12-13 12:16:04 +0000

    change app.js file

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-	<h1 className="App-title">Wlecome to React!!!!</h1>
-      <header>
-        <p className="App-intro">
-		I was chengrede.
-        </p>
+        <p>Bye there!</p>
+        <p> Hi its me neew code</p>
+      </header>
     </div>
   );
 }
 
 export default App;
+
